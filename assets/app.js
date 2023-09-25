52c1ead728f3b11f0a4c717c5ba500cd037517ca
Pop <pop.mickael@gmail.com>
2023-09-26 00:45:29 +0200

    feat: upgrade symfony from 6.2 to 6.3

--- assets/app.js
+++ assets/app.js
@@ -6,13 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import 'bootstrap';
-import './styles/app.scss';
+import './styles/app.css';
 
-import './js/search.js';
-import './js/cart.js';
-
-
-
-
-
+// start the Stimulus application
+import './bootstrap';
